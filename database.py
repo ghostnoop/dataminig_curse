41cded78344fcfb7ffe64a5c6ab6540e2ec897a4
Gilyazov Marat <giliyazovmarat@gmail.com>
2021-03-04 20:43:49 +0300

    upd

---
+++ database.py
@@ -0,0 +1,36 @@
+from abc import ABC
+
+import asyncpg
+
+
+class ConfigurableDB(ABC):
+    pool: asyncpg.pool.Pool
+
+    @classmethod
+    def configurate(cls, pool: asyncpg.pool.Pool):
+        cls.pool = pool
+
+
+class TopWords(ConfigurableDB):
+    CREATE_TABLE = (
+        "CREATE TABLE IF NOT EXISTS top_words "
+        "( "
+        "id SERIAL PRIMARY KEY "
+        "text VARCHAR(256),"
+        "result_of INTEGER "
+        "); "
+    )
+
+    @classmethod
+    async def create_table(cls):
+        await cls.pool.execute(cls.CREATE_TABLE)
+
+
+
+
+async def preapare_db(*args, **kwargs):
+    pool = await asyncpg.create_pool(user="postgres", password="postgres",
+                                     database="datamining", host="localhost")
+
+    TopWords.configurate(pool)
+    await TopWords.create_table()
--- test.py
+++ test.py
@@ -1,1 +1,3 @@
-print("test")
+a = {'a': 1, 'b': 2}
+for k, v in a.items():
+    print(k, v)
